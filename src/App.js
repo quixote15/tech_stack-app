e0246ec5f22a44ca1073643c53670aa131cd3810
caricaDev <santos2tiago@gmail.com>
2019-03-18 16:07:30 -0300

    altter components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import {Text, View} from 'react-native';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import {reducers} from './reducers' 
-import Header from './components/Header'
+import Header from './components/common/Header'
+import LibraryList from './components/LibraryList';
 
 export default class App extends Component{
   render() {
@@ -12,6 +13,7 @@ export default class App extends Component{
       <Provider store={createStore(reducers)}>
         <View>
           <Header headerText="Tech Stack"></Header>
+          <LibraryList/>
         </View>
       </Provider>
     );
