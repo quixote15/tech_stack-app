90e8a049a6d8ebc6be4850fc6aa1e9501b9a60d8
caricaDev <santos2tiago@gmail.com>
2019-03-18 00:28:06 -0300

    config app with src folder

---
+++ src/App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export default class App extends Component{
+  render() {
+    return (
+      <View>
+        <Text>Welcome to React Native!</Text>
+      </View>
+    );
+  }
+}
+
