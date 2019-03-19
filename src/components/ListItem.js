7c0cf483ae35b47059bf7984810e4abe00fe5bb9
caricaDev <santos2tiago@gmail.com>
2019-03-18 23:42:44 -0300

    add map state function

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react'
-import {Text} from 'react-native'
-import CardSection from './common/CardSection'
+import {Text, TouchableWithoutFeedback, View} from 'react-native'
+import CardSection from './common/CardSection';
+import * as actions from '../actions';
+import {connect} from 'react-redux'
+
 
 class ListItem extends Component {
+
+    renderDescription(){
+        const {library, selectedLibraryId}  = this.props
+        if(library.id === selectedLibraryId)
+            return <Text>{library.description}</Text>
+    }
+    
     render(){
+        const {id, title} = this.props.library;
+
         return (
-            <CardSection >
-                <Text style={styles.textStyle}>{this.props.library.title}</Text>
-            </CardSection>
+            <TouchableWithoutFeedback onPress={()=> this.props.selectLibrary(id)}>
+                <View>
+                    <CardSection >
+                        <Text style={styles.textStyle}>{title}</Text>
+                    </CardSection>
+
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
         )
     }
 }
@@ -19,4 +37,9 @@ const styles = {
         paddingLeft: 10,
     }
 }
-export default ListItem
+
+const mapStatetoProps = (state) => {
+    return {selectedLibraryId: state.selectedLibraryId}
+}
+
+export default connect(mapStatetoProps, actions)(ListItem)
